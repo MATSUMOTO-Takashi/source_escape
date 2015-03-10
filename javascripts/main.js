1f8e872ce4a338697fa8418f7c63fac72f47594e
MATSUMOTO-Takashi <t.matsumoto0308@gmail.com>
2015-03-11 01:04:06 +0900

    コメント付けた

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,16 @@
 $(function() {
+  /*
+   * 変換ボタンが押されたときに、入力されている文字列をHTMLエスケープした上で、preタグで括って出力します。
+   */
   $('#convert').on('click', function() {
     var src = $('#before-src').val();
     var output = $('<div/>').append($('<pre class="prettyprint linenums"/>').text(src)).html();
     $('#after-src').val(output);
   });
 
+  /*
+   * 変換後のテキストエリアにフォーカスされたときに、テキストを選択します。
+   */
   $('#after-src').on('focus', function() {
     $(this).select();
   });
